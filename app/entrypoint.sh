59495d742ceaf9646d0b5b745562700baec2491e
Kim Jeker <github@kije.ch>
2016-05-01 11:48:37 +0200

    Used $DH_PARAM_BITS environment var for dhparam generation

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -59,7 +59,7 @@ function check_writable_directory {
 function check_dh_group {
     if [[ ! -f /etc/nginx/certs/dhparam.pem ]]; then
         echo "Creating Diffie-Hellman group (can take several minutes...)"
-        openssl dhparam -out /etc/nginx/certs/.dhparam.pem.tmp 2048
+        openssl dhparam -out /etc/nginx/certs/.dhparam.pem.tmp $DH_PARAM_BITS
         mv /etc/nginx/certs/.dhparam.pem.tmp /etc/nginx/certs/dhparam.pem || exit 1
     fi
 }
