87243870388bc8e5a5716dde4a183a6f1d77293b
Yves Blusseau <90z7oey02@sneakemail.com>
2016-12-30 13:28:08 +0100

    Use the acme-0.8 branch of simp_le

--- install_simp_le.sh
+++ install_simp_le.sh
@@ -6,12 +6,12 @@ set -e
 apk --update add python py-requests py-setuptools git gcc py-pip musl-dev libffi-dev python-dev openssl-dev
 
 # Get Let's Encrypt simp_le client source
+branch="acme-0.8"
 mkdir -p /src
-git -C /src clone https://github.com/kuba/simp_le.git
+git -C /src clone --depth=1 -b $branch https://github.com/kuba/simp_le.git
 
 # Install simp_le in /usr/bin
 cd /src/simp_le
-git checkout acme-0.8
 python ./setup.py install
 
 # Make house cleaning
