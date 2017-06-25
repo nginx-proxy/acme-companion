33e7b59591c2733cc08094a9279fd0f8185155eb
yalamerde <almereyda@allmende.io>
2017-06-25 01:52:28 +0100

    update simp_le to master (>=0.3.0)

--- install_simp_le.sh
+++ install_simp_le.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install python packages needed to build simp_le
-apk --update add python py-requests py-setuptools git gcc py-pip musl-dev libffi-dev python-dev openssl-dev
+apk --update add python py-setuptools git gcc py-pip musl-dev libffi-dev python-dev openssl-dev
 
 # Get Let's Encrypt simp_le client source
 branch="master"
@@ -12,7 +12,12 @@ git -C /src clone --depth=1 --branch $branch https://github.com/zenhack/simp_le.
 
 # Install simp_le in /usr/bin
 cd /src/simp_le
-python ./setup.py install
+#pip install wheel requests
+for pkg in pip distribute setuptools wheel
+do
+  pip install -U "${pkg?}"
+done
+pip install .
 
 # Make house cleaning
 cd /
