fb3830952ce17d9d198d2399339065118eea8ef8
Nicolas Duchon <nicolas.duchon@gmail.com>
2018-06-01 10:33:12 +0200

    Fix and extend docker_api test (#387)

--- test/tests/test-functions.sh
+++ test/tests/test-functions.sh
@@ -12,10 +12,16 @@ export -f get_base_domain
 function run_le_container {
   local image="${1:?}"
   local name="${2:?}"
+  if [[ "$SETUP" == '3containers' ]]; then
+    docker_gen_arg="--env NGINX_DOCKER_GEN_CONTAINER=$DOCKER_GEN_CONTAINER_NAME"
+  else
+    docker_gen_arg=""
+  fi
   docker run -d \
     --name "$name" \
     --volumes-from $NGINX_CONTAINER_NAME \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    $docker_gen_arg \
     --env "DEBUG=true" \
     --env "ACME_CA_URI=http://${BOULDER_IP}:4000/directory" \
     --label com.github.jrcs.letsencrypt_nginx_proxy_companion.test_suite \
