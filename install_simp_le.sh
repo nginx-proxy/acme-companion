b89be7eae413578e7b3004e2ef0738834f21e4d4
Nicolas Duchon <nicolas.duchon@gmail.com>
2019-05-16 10:42:32 +0200

    Switch to Python 3 (#540)
    Python 2.7 will reach its end of life on 1st Jan 2020

--- install_simp_le.sh
+++ install_simp_le.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-# Install python packages needed to build simp_le
-apk --update add python py-setuptools git gcc py-pip musl-dev libffi-dev python-dev openssl-dev
+# Install python and packages needed to build simp_le
+apk add --update python3 git gcc musl-dev libffi-dev python3-dev openssl-dev
+
+# Create expected symlinks if they don't exist
+[[ -e /usr/bin/pip ]] || ln -sf /usr/bin/pip3 /usr/bin/pip
+[[ -e /usr/bin/python ]] || ln -sf /usr/bin/python3 /usr/bin/python
 
 # Get Let's Encrypt simp_le client source
 branch="0.14.0"
@@ -13,14 +17,14 @@ git -C /src clone --depth=1 --branch $branch https://github.com/zenhack/simp_le.
 # Install simp_le in /usr/bin
 cd /src/simp_le
 #pip install wheel requests
-for pkg in pip distribute setuptools wheel
+for pkg in pip setuptools wheel
 do
-  pip install -U "${pkg?}"
+  pip3 install -U "${pkg?}"
 done
-pip install .
+pip3 install .
 
 # Make house cleaning
 cd /
 rm -rf /src
-apk del git gcc py-pip musl-dev libffi-dev python-dev openssl-dev
+apk del git gcc musl-dev libffi-dev python3-dev openssl-dev
 rm -rf /var/cache/apk/*
