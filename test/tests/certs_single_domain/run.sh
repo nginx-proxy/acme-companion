000c279bb6c906b86d4d5a8855d8fe46394f7900
Nicolas Duchon <nicolas.duchon@gmail.com>
2020-12-22 00:07:15 +0100

    CI/CD: Use in-container artifact cleaning script

--- test/tests/certs_single_domain/run.sh
+++ test/tests/certs_single_domain/run.sh
@@ -107,8 +107,7 @@ for hosts in "${letsencrypt_hosts[@]}"; do
   done
 
   docker stop "$container" &> /dev/null
-  docker exec "$le_container_name" rm -rf /etc/nginx/certs/le?.wtf*
-  docker exec "$le_container_name" rm -rf /etc/acme.sh/default/le?.wtf*
+  docker exec "$le_container_name" /app/cleanup_test_artifacts --default-cert
   i=$(( i + 1 ))
 
 done
