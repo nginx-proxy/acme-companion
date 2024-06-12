494a248266fcbd1bd80b8251c7d78f47a6485b7b
Nicolas Duchon <nicolas.duchon@gmail.com>
2024-06-12 07:35:36 +0200

    test: remove /etc/nginx/conf.d volume

--- test/setup/setup-nginx-proxy.sh
+++ test/setup/setup-nginx-proxy.sh
@@ -24,7 +24,6 @@ case $SETUP in
     docker run -d -p 80:80 -p 443:443 \
       --name "$NGINX_CONTAINER_NAME" \
       -v /etc/nginx/certs \
-      -v /etc/nginx/conf.d \
       -v /etc/nginx/vhost.d \
       -v /usr/share/nginx/html \
       -v /var/run/docker.sock:/tmp/docker.sock:ro \
