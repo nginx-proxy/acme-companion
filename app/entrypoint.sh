182b89c0b8d183fa409da18c60e719e93c400ff2
Nicolas Duchon <nicolas.duchon@gmail.com>
2025-01-18 17:45:02 +0100

    refactor: whitespace typo

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -14,7 +14,7 @@ function print_version {
 function check_docker_socket {
     if [[ $DOCKER_HOST == unix://* ]]; then
         socket_file=${DOCKER_HOST#unix://}
-	    if [[ ! -S $socket_file ]]; then
+	if [[ ! -S $socket_file ]]; then
             echo "Error: you need to share your Docker host socket with a volume at $socket_file" >&2
             echo "Typically you should run your container with: '-v /var/run/docker.sock:$socket_file:ro'" >&2
             exit 1
